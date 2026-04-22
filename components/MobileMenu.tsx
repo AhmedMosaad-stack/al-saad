06d520ba36dc4a4d1277ea3bb78b3ed43794c479
Ahmed Mosaad <ahmedmosad840@gmail.com>
2026-04-22 19:30:11 +0200

    Mobile menubar order fixed

--- components/MobileMenu.tsx
+++ components/MobileMenu.tsx
@@ -2,10 +2,10 @@ export default function MobileMenu() {
   return (
     <div id="mob-menu">
       <a href="#about">About</a>
-      <a href="#legacy">Legacy</a>
       <a href="#finishing">Finishing</a>
       <a href="#projects">Projects</a>
       <a href="#services">Vision</a>
+      <a href="#legacy">Legacy</a>
       <a href="#contact">Contact</a>
     </div>
   )
